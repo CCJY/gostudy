74776c7ea5dd44a2e6a60d91829e14537222f298
Junyoung Choi <ccjy1113@gmail.com>
2021-03-04 19:01:18 -0500

    structs

--- study/structs.go
+++ study/structs.go
@@ -6,33 +6,51 @@ import (
 )
 
 const usixteenbitmax float64 = 65535
-const kmh_multiple float64 = 1.60934
+const kmhMultiple float64 = 1.60934
 
-type car struct {
-	Brake_pedal    uint16  `json:"brake_pedal"`
-	Steering_whell int16   `json:"steering_whell"` // -32k - +32K
-	Top_speed_kmh  float64 `json:"top_speed_kmh"`
-	Gas_pedal      uint16  `json:"gas_pedal"` // min 0 max 65535
+// Car of struct is an example
+type Car struct {
+	BrakePedal    uint16  `json:"brake_pedal"`
+	SteeringWhell int16   `json:"steering_whell"` // -32k - +32K
+	TopSpeedKmh   float64 `json:"top_speed_kmh"`
+	GasPedal      uint16  `json:"gas_pedal"` // min 0 max 65535
 }
 
-func (c car) kmh() float64 {
-	return float64(c.Gas_pedal) * (c.Top_speed_kmh / usixteenbitmax)
+// Value Receivers
+func (c Car) kmh() float64 {
+	return float64(c.GasPedal) * (c.TopSpeedKmh / usixteenbitmax)
+}
+
+// Pointer Receivers
+func (c *Car) newTopSpeed(newspeed float64) {
+	c.TopSpeedKmh = newspeed
+}
+
+// This should be not changed
+func (c Car) newTopSpeedNotPointer(newspeed float64) {
+	c.TopSpeedKmh = newspeed
+}
+
+func (c *Car) toJSONString() string {
+	b, err := json.Marshal(c)
+	if err != nil {
+		return err.Error()
+	}
+	return string(b)
 }
 
 func main() {
-	a_car := &car{Gas_pedal: 2234,
-		Brake_pedal: 0, Steering_whell: 12561,
-		Top_speed_kmh: 225.0,
+	aCar := &Car{GasPedal: 2234,
+		BrakePedal: 0, SteeringWhell: 12561,
+		TopSpeedKmh: 225.0,
 	}
 	// a_car = &car{2234, 0, 12561, 225.0}
 
-	fmt.Println(a_car.kmh())
-
-	b, err := json.Marshal(a_car)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(b))
-
+	fmt.Println(aCar.kmh())
+	fmt.Println(aCar.toJSONString())
+	aCar.newTopSpeed(100)
+	fmt.Println(aCar.toJSONString())
+	// not changed
+	aCar.newTopSpeedNotPointer(150)
+	fmt.Println(aCar.toJSONString())
 }
