cb37d79683d88629bfc1158392a2ac12a9ec5e68
Junyoung Choi <ccjy1113@gmail.com>
2021-03-04 19:57:22 -0500

    add marshal,unmarshal

--- study/accessing_internet.go
+++ study/accessing_internet.go
@@ -1,21 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func getTestRestAPI(url string) {
+type todo struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func getBodyBytesFromAPI(url string) []byte {
 	resp, _ := http.Get(url)
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
 	resp.Body.Close()
+	return bytes
+}
 
+func (o *todo) toString() string {
+	return fmt.Sprintf("UserID=%d ID=%d Title=%s Completed=%t",
+		o.UserID, o.ID, o.Title, o.Completed)
+}
+
+func (o *todo) toJSONString() (string, error) {
+	b, err := json.Marshal(o)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func todoFromBytes(bytes []byte) (*todo, error) {
+	t := new(todo)
+	err := json.Unmarshal(bytes, t)
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
 }
 
 func main() {
 	var url string = "https://jsonplaceholder.typicode.com/todos/1"
-	getTestRestAPI(url)
+	var bytes []byte = getBodyBytesFromAPI(url)
+
+	t, err := todoFromBytes(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(t.toJSONString())
+	fmt.Println(t.toString())
 }
